19a96ca005caa24a6468913922c7d17bb00419be
Victin <victorgomezes@hotmail.com>
2017-09-21 13:49:47 +0200

    Segunda subida

--- ComboBox/ComboBox/Form1.Designer.vb
+++ ComboBox/ComboBox/Form1.Designer.vb
@@ -22,9 +22,51 @@ Partial Class Form1
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.cboDatos = New System.Windows.Forms.ComboBox()
+        Me.lstDatos = New System.Windows.Forms.ListBox()
+        Me.btnCargar = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'cboDatos
+        '
+        Me.cboDatos.FormattingEnabled = True
+        Me.cboDatos.Location = New System.Drawing.Point(12, 241)
+        Me.cboDatos.Name = "cboDatos"
+        Me.cboDatos.Size = New System.Drawing.Size(121, 21)
+        Me.cboDatos.TabIndex = 0
+        '
+        'lstDatos
+        '
+        Me.lstDatos.FormattingEnabled = True
+        Me.lstDatos.Location = New System.Drawing.Point(13, 12)
+        Me.lstDatos.Name = "lstDatos"
+        Me.lstDatos.Size = New System.Drawing.Size(120, 212)
+        Me.lstDatos.TabIndex = 1
+        '
+        'btnCargar
+        '
+        Me.btnCargar.Location = New System.Drawing.Point(166, 12)
+        Me.btnCargar.Name = "btnCargar"
+        Me.btnCargar.Size = New System.Drawing.Size(172, 23)
+        Me.btnCargar.TabIndex = 2
+        Me.btnCargar.Text = "Cargar"
+        Me.btnCargar.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(832, 274)
+        Me.Controls.Add(Me.btnCargar)
+        Me.Controls.Add(Me.lstDatos)
+        Me.Controls.Add(Me.cboDatos)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents cboDatos As System.Windows.Forms.ComboBox
+    Friend WithEvents lstDatos As ListBox
+    Friend WithEvents btnCargar As Button
 End Class
