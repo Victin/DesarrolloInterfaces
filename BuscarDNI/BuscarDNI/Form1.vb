a9935b98488494eaf42c1070bc057b6537cf51a2
Victin <victorgomezes@hotmail.com>
2017-09-28 13:07:48 +0200

    Buscar DNI v1

--- BuscarDNI/BuscarDNI/Form1.vb
+++ BuscarDNI/BuscarDNI/Form1.vb
@@ -1,27 +1,26 @@
 ﻿Public Class Form1
+
+    Dim i As Integer = 0
+
     Private Sub btnAñadir_Click(sender As Object, e As EventArgs) Handles btnAñadir.Click
         For Each Controles In Controls
             If TypeOf (Controles) Is TextBox Then
                 If Controles.Text = "" Then
-                    If txtDNI.Text = "" Then
-                        MsgBox("Rellena el DNI")
-                    End If
-                    If txtNom.Text = "" Then
-                        MsgBox("Rellena el nombre")
-                    End If
-                    If txtApp1.Text = "" Then
-                        MsgBox("Rellena el primer apellido")
-                    End If
-                    If txtApp2.Text = "" Then
-                        MsgBox("Rellena el segundo apellido")
-                    End If
-                    If txtDNI.Text = "" Then
-                        MsgBox("Rellena el nombre")
-                    End If
-
+                    MsgBox("Falta uno o varios campos por introducir")
+                    Controles.focus()
                 End If
             End If
         Next
+        ReDim Preserve agenda(i)
+        agenda(i).dni = txtDNI.Text
+        agenda(i).nombre = txtNom.Text
+        agenda(i).app1 = txtApp1.Text
+        agenda(i).app2 = txtApp2.Text
+        agenda(i).dir = txtDir.Text
+        agenda(i).tlf = txtTlf.Text
+
+        i += 1
+        blanco()
     End Sub
 
     Private Sub blanco()
@@ -30,5 +29,9 @@
                 Controles.Text = ""
             End If
         Next
+    End Sub
+
+    Private Sub btnSalir_Click(sender As Object, e As EventArgs) Handles btnSalir.Click
+        Me.Close()
     End Sub
 End Class
---
+++ BuscarDNI/BuscarDNI/Module1.vb
@@ -0,0 +1,11 @@
+﻿Module Module1
+    Public Structure registro
+        Public dni As String
+        Public nombre As String
+        Public app1 As String
+        Public app2 As String
+        Public dir As String
+        Public tlf As Integer
+    End Structure
+    Public agenda() As registro
+End Module
