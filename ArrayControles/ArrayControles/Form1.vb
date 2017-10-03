42634f3d94e217ef3dd71b0603057c5441c41239
Viti <victorgomezes@hotmail.com>
2017-10-03 13:11:35 +0200

    ArrayControles v1

---
+++ ArrayControles/ArrayControles/Form1.vb
@@ -0,0 +1,28 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        For i = 0 To 5
+            Dim miTxt As New TextBox
+            miTxt.Name = "txt" & i
+            miTxt.Tag = i
+            miTxt.Size = New Size(100, 20)
+            miTxt.Location = New Point(30, 20 * (i + 1)) '+ Añadir la distancia al principio
+            miTxt.Text = miTxt.Name
+            Me.Controls.Add(miTxt)
+        Next
+
+        For i = 0 To 5
+            Dim miBtn As New Button
+            miBtn.Name = "btn" & i
+            miBtn.Tag = i
+            miBtn.Size = New Size(100, 20)
+            miBtn.Location = New Point(150, 20 * (i + 1)) '+ Añadir la distancia al principio
+            miBtn.Text = miBtn.Name
+            Me.Controls.Add(miBtn)
+            AddHandler miBtn.Click, AddressOf Prueba
+        Next
+    End Sub
+
+    Private Sub Prueba(ByVal Sender As Object, ByVal e As System.EventArgs)
+        MsgBox("Control " + CType(Sender, Button).Tag.ToString()) 'Recoge el indice de donde has pulsado
+    End Sub
+End Class
